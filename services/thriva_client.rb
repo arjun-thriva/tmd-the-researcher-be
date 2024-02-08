25789c27ace23a575a6f26fc16d2a98855378702
Arjun Raja <arjun@thriva.co>
2024-02-08 12:10:18 +0000

    more method

--- services/thriva_client.rb
+++ services/thriva_client.rb
@@ -2,8 +2,9 @@ class ThrivaClient
   def initialize(auth0_client)
     @auth0_client = auth0_client
   end
-  def get_order(reference_id)
-    url = URI("https://api.euw2.development.thriva.io/v1/orders/#{reference_id}")
+
+  def get_order(order_reference_id)
+    url = URI("https://api.euw2.development.thriva.io/v1/orders/#{order_reference_id}")
 
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
@@ -14,5 +15,27 @@ class ThrivaClient
     request["Authorization"] = "Bearer #{@auth0_client.generate_access_token}"
   end
 
-  def
+  def get_order_tests(order_reference_id)
+    url = URI("https://api.euw2.development.thriva.io/v1/orders/#{order_reference_id}/tests")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Accept"] = "application/vnd.api+json"
+    request["Content-Type"] = "application/vnd.api+json"
+    request["Authorization"] = "Bearer #{@auth0_client.generate_access_token}"
+  end
+
+  def get test_result_set(result_set_reference_id)
+    url = URI("https://api.euw2.development.thriva.io/v1/result-sets/#{result_set_reference_id}")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Get.new(url)
+    request["Accept"] = "application/vnd.api+json"
+    request["Content-Type"] = "application/vnd.api+json"
+    request["Authorization"] = "Bearer #{@auth0_client.generate_access_token}"
+  end
 end
