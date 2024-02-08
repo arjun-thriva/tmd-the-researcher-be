a49bf40d00074321d63461473c07ba6a023bd8c1
james g <jamesgrigg01@gmail.com>
2024-02-08 14:31:02 +0000

    get ids

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
-
+require './services/order_fetcher'
+require './services/thriva_auth_client'
 get '/' do
   'Hello world!'
+end
+
+client_id = ENV['THRIVA_AUTH0_CLIENT_ID']
+client_secret = ENV['THRIVA_AUTH0_CLIENT_SECRET']
+
+ORDER_FETCHER = OrderFetcher.new(client_id, client_secret)
+
+get '/fetch_orders' do
+  # Example start and end dates
+  start_date = "2022-12-30"
+  end_date = "2023-01-12"
+
+  # Fetch the orders
+  order_ids = ORDER_FETCHER.fetch_orders(start_date, end_date)
+
+  # Respond with the order IDs in JSON format
+  content_type :json
+  { order_ids: order_ids }.to_json
 end
--- services/order_fetcher.rb
+++ services/order_fetcher.rb
@@ -1,14 +1,18 @@
 require 'net/http'
 require 'uri'
+require 'logger'
+
 require 'json'
 
 class OrderFetcher
   def initialize(client_id, client_secret)
+    @logger = Logger.new(STDOUT)
     @auth_client = ThrivaAuthClient.new(client_id, client_secret)
   end
 
   def fetch_orders(start_date, end_date)
     access_token = @auth_client.generate_access_token
+    @logger.info("HTTP #{access_token}")
     base_url = "https://api.euw2.development.thriva.io/v1/orders/"
     query_params = "?filter[startDate]=#{start_date}&filter[endDate]=#{end_date}"
     url = URI(base_url + query_params)
@@ -18,9 +22,17 @@ class OrderFetcher
 
     request = Net::HTTP::Get.new(url)
     request["Authorization"] = "Bearer #{access_token}"
-    request["Content-Type"] = "application/json"
+    request["Content-Type"] = "application/vnd.api+json"
+    request["Accept"] = "application/vnd.api+json"
 
+    
     response = https.request(request)
-    JSON.parse(response.read_body)
+
+    @logger.info("HTTP Response Status: #{response.code} #{response.message}")
+    @logger.debug("HTTP Response Body: #{response.body}")
+  
+    parsed_response = JSON.parse(response.read_body)
+    order_ids = parsed_response["data"].map { |order| order["id"] }
+    order_ids
   end
 end
